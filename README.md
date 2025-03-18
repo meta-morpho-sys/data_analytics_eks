941031dbb721f14e3d813c29bae8c4a816a9f122
yuliya rothman <yuliya.nedyalkova777@gmail.com>
2025-03-18 19:00:25 +0000

    adds route tables

---
+++ README.md
@@ -0,0 +1,4 @@
+# Data Analytics EKS Cluster
+
+This repository contains the code to create an EKS cluster with a managed node group using Terraform.
+It is based on a case study for an interview process.
--- variables.tf
+++ variables.tf
@@ -1,9 +1,9 @@
 variable "project" {
-    type = string                     # The type of the variable, in this case a string
-    default = "eks-analytics"                 # Default value for the variable
+  type    = string          # The type of the variable, in this case a string
+  default = "eks-analytics" # Default value for the variable
 }
 
 variable "region" {
-    type = string
-    default = "eu-west-1"
+  type    = string
+  default = "eu-west-1"
 }
